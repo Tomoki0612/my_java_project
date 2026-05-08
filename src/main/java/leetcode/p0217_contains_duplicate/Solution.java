30cb1cec31cdd045a1ce979f41b399b3191dc7a9
Tomoki <tomozou4495@gmail.com>
2026-05-08 18:56:17 +0900

    scaffold: #217 Contains Duplicate

---
+++ src/main/java/leetcode/p0217_contains_duplicate/Solution.java
@@ -0,0 +1,52 @@
+/*
+ * Contains Duplicate
+ * Difficulty: Easy
+ * https://leetcode.com/problems/contains-duplicate/
+ *
+ * 整数配列 `nums` が与えられたとき、
+ * 配列内のいずれかの値が少なくとも2回出現する場合は `true` を、
+ * すべての要素が異なる場合は `false` を返してください。
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3,1]
+ *
+ * Output: true
+ *
+ * Explanation:
+ *
+ * 要素 1 がインデックス 0 と 3 に出現します。
+ *
+ * Example 2:
+ *
+ * Input: nums = [1,2,3,4]
+ *
+ * Output: false
+ *
+ * Explanation:
+ *
+ * すべての要素が異なります。
+ *
+ * Example 3:
+ *
+ * Input: nums = [1,1,1,3,3,4,3,2,4,2]
+ *
+ * Output: true
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 	  - `1 <= nums.length <= 105`
+ *
+ * 	  - `-109 <= nums[i] <= 109`
+ */
+package leetcode.p0217_contains_duplicate;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        return false;
+    }
+}
---
+++ src/test/java/leetcode/p0217_contains_duplicate/SolutionTest.java
@@ -0,0 +1,14 @@
+package leetcode.p0217_contains_duplicate;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+// Contains Duplicate [Easy]
+class SolutionTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    void example1() {
+        // TODO: add test cases
+    }
+}
