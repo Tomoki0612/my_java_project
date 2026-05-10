db2bd7ed670cb2c3bf18f2d277fe4c23dfb4e02c
Tomoki <tomozou4495@gmail.com>
2026-05-10 10:58:03 +0900

    scaffold: #222 Count Complete Tree Nodes

---
+++ src/test/java/leetcode/p0222_count_complete_tree_nodes/SolutionTest.java
@@ -0,0 +1,30 @@
+package leetcode.p0222_count_complete_tree_nodes;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+// Count Complete Tree Nodes [Easy]
+class SolutionTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    void example1() {
+        // Input:  root = [1,2,3,4,5,6]
+        // Output: 6
+        // assertEquals(expected, solution.countNodes(...));
+    }
+
+    @Test
+    void example2() {
+        // Input:  root = []
+        // Output: 0
+        // assertEquals(expected, solution.countNodes(...));
+    }
+
+    @Test
+    void example3() {
+        // Input:  root = [1]
+        // Output: 1
+        // assertEquals(expected, solution.countNodes(...));
+    }
+}
