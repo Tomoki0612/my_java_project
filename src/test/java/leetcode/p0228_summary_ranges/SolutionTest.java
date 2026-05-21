303fe7d7f57b734b486800a7f3dfc818582940ed
Tomoki <tomozou4495@gmail.com>
2026-05-21 12:13:34 +0900

    scaffold: #228 Summary Ranges

---
+++ src/test/java/leetcode/p0228_summary_ranges/SolutionTest.java
@@ -0,0 +1,23 @@
+package leetcode.p0228_summary_ranges;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+// Summary Ranges [Easy]
+class SolutionTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    void example1() {
+        // Input:  nums = [0,1,2,4,5,7]
+        // Output: [&quot;0->2&quot;,&quot;4->5&quot;,&quot;7&quot;]
+        // assertEquals(expected, solution.summaryRanges(...));
+    }
+
+    @Test
+    void example2() {
+        // Input:  nums = [0,2,3,4,6,8,9]
+        // Output: [&quot;0&quot;,&quot;2->4&quot;,&quot;6&quot;,&quot;8->9&quot;]
+        // assertEquals(expected, solution.summaryRanges(...));
+    }
+}
