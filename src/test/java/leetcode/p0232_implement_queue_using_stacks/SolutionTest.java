be15eed73d6f58c962ced2a74bacb85721440312
Tomoki <tomozou4495@gmail.com>
2026-05-26 20:09:52 +0900

    scaffold: #232 Implement Queue using Stacks

---
+++ src/test/java/leetcode/p0232_implement_queue_using_stacks/SolutionTest.java
@@ -0,0 +1,14 @@
+package leetcode.p0232_implement_queue_using_stacks;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+// Implement Queue using Stacks [Easy]
+class SolutionTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    void example1() {
+        // TODO: add test cases
+    }
+}
