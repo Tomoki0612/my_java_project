3e4408ef4e7d803f99817ab1c26ac48a2a09cec8
Tomoki <tomozou4495@gmail.com>
2026-05-28 18:57:19 +0900

    scaffold: #234 Palindrome Linked List

---
+++ src/test/java/leetcode/p0234_palindrome_linked_list/SolutionTest.java
@@ -0,0 +1,23 @@
+package leetcode.p0234_palindrome_linked_list;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+// Palindrome Linked List [Easy]
+class SolutionTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    void example1() {
+        // Input:  head = [1,2,2,1]
+        // Output: true
+        // assertEquals(expected, solution.isPalindrome(...));
+    }
+
+    @Test
+    void example2() {
+        // Input:  head = [1,2]
+        // Output: false
+        // assertEquals(expected, solution.isPalindrome(...));
+    }
+}
