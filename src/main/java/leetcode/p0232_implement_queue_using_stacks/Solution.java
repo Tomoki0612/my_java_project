4de0747adabb204d2f3fb5283be28a93d9ed8ba7
Tomoki <tomozou4495@gmail.com>
2026-06-01 10:15:23 +0900

    progress: #232 Implement Queue using Stacks → review (stage 2)

--- src/main/java/leetcode/p0232_implement_queue_using_stacks/Solution.java
+++ src/main/java/leetcode/p0232_implement_queue_using_stacks/Solution.java
@@ -66,23 +66,23 @@
  */
 package leetcode.p0232_implement_queue_using_stacks;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 class MyQueue {
     private Deque<Integer> stack1;
     private Deque<Integer> stack2;
+    
     public MyQueue() {
         stack1 = new ArrayDeque<>();
         stack2 = new ArrayDeque<>();
     }
     
     public void push(int x) {
-        while (stack1.size() != 0) {
+        while (!stack1.isEmpty()) {
             stack2.push(stack1.pop());
         }
         stack1.push(x);
-        while (stack2.size() != 0) {
+        while (!stack2.isEmpty()) {
             stack1.push(stack2.pop());
         }
     }
