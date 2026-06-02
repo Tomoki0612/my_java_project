6f8b2d1de79d2d7518606f1fc5ccb2574268917a
Tomoki <tomozou4495@gmail.com>
2026-06-02 20:43:26 +0900

    progress: #242 Valid Anagram → mastered

--- src/main/java/leetcode/p0242_valid_anagram/Solution.java
+++ src/main/java/leetcode/p0242_valid_anagram/Solution.java
@@ -36,8 +36,21 @@
  */
 package leetcode.p0242_valid_anagram;
 
+import java.util.*;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        return false;
+        
+        List<Character> list = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            list.add(s.charAt(i));
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            if (list.contains(t.charAt(i))) {
+                list.remove(list.indexOf(t.charAt(i)));
+            } else return false;
+        }
+        return list.size() == 0;
     }
 }
