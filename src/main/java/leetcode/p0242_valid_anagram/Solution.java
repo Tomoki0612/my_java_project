ad6ae6613b46535a1baa6b92316089f926296c1b
Tomoki <tomozou4495@gmail.com>
2026-06-02 20:01:17 +0900

    scaffold: #242 Valid Anagram

---
+++ src/main/java/leetcode/p0242_valid_anagram/Solution.java
@@ -0,0 +1,43 @@
+/*
+ * Valid Anagram
+ * Difficulty: Easy
+ * https://leetcode.com/problems/valid-anagram/
+ *
+ * 2つの文字列 `s` と `t` が与えられたとき、
+ * `t` が `s` のアナグラムであれば `true` を、
+ * そうでなければ `false` を返してください。
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "anagram", t = "nagaram"
+ *
+ * Output: true
+ *
+ * Example 2:
+ *
+ * Input: s = "rat", t = "car"
+ *
+ * Output: false
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 	  - `1 <= s.length, t.length <= 5 * 104`
+ *
+ * 	  - `s` と `t` は英小文字のみで構成されます。
+ *
+ *
+ *
+ * Follow up: 入力にUnicode文字が含まれる場合はどうしますか？
+ * そのような場合に対応するために、解法をどう変更しますか？
+ */
+package leetcode.p0242_valid_anagram;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        return false;
+    }
+}
