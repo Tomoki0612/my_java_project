205733f6ba5ed1ba73a48d2bed00dfdb2e935bfa
Tomoki <tomozou4495@gmail.com>
2026-06-04 19:59:51 +0900

    scaffold: #257 Binary Tree Paths

---
+++ src/test/java/leetcode/p0257_binary_tree_paths/SolutionTest.java
@@ -0,0 +1,23 @@
+package leetcode.p0257_binary_tree_paths;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+// Binary Tree Paths [Easy]
+class SolutionTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    void example1() {
+        // Input:  root = [1,2,3,null,5]
+        // Output: [&quot;1->2->5&quot;,&quot;1->3&quot;]
+        // assertEquals(expected, solution.binaryTreePaths(...));
+    }
+
+    @Test
+    void example2() {
+        // Input:  root = [1]
+        // Output: [&quot;1&quot;]
+        // assertEquals(expected, solution.binaryTreePaths(...));
+    }
+}
