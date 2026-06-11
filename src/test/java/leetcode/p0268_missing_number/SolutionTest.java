c85588399ad552573268afa057e36324e6bbbcd2
Tomoki <tomozou4495@gmail.com>
2026-06-11 21:14:50 +0900

    scaffold: #268 Missing Number

---
+++ src/test/java/leetcode/p0268_missing_number/SolutionTest.java
@@ -0,0 +1,14 @@
+package leetcode.p0268_missing_number;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+// Missing Number [Easy]
+class SolutionTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    void example1() {
+        // TODO: add test cases
+    }
+}
