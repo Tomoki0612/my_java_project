083686e1f9717d3266ca1f676a4d2dfbcc96e4cb
Tomoki <tomozou4495@gmail.com>
2026-06-30 19:42:35 +0900

    scaffold: #283 Move Zeroes

---
+++ src/test/java/leetcode/p0283_move_zeroes/SolutionTest.java
@@ -0,0 +1,23 @@
+package leetcode.p0283_move_zeroes;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+// Move Zeroes [Easy]
+class SolutionTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    void example1() {
+        // Input:  nums = [0,1,0,3,12]
+        // Output: [1,3,12,0,0]
+        // assertEquals(expected, solution.moveZeroes(...));
+    }
+
+    @Test
+    void example2() {
+        // Input:  nums = [0]
+        // Output: [0]
+        // assertEquals(expected, solution.moveZeroes(...));
+    }
+}
