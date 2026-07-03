481736a7d4369543f72d0e270f8cb6135d71a45c
Tomoki <tomozou4495@gmail.com>
2026-07-03 19:04:11 +0900

    progress: #349 Intersection of Two Arrays → mastered

--- src/main/java/leetcode/p0349_intersection_of_two_arrays/Solution.java
+++ src/main/java/leetcode/p0349_intersection_of_two_arrays/Solution.java
@@ -30,8 +30,25 @@
  */
 package leetcode.p0349_intersection_of_two_arrays;
 
+import java.util.*;
+
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        return null;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    set.add(nums1[i]);
+                }
+            }
+        }
+
+        int[] result = new int[set.size()];
+        int k = 0;
+        for (int s : set) {
+            result[k] = s;
+            k++;
+        }
+        return result;
     }
 }
