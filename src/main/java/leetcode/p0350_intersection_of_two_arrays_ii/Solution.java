900f37888b6350cd18c4e2c007706b39294cd34d
Tomoki <tomozou4495@gmail.com>
2026-07-04 12:04:58 +0900

    progress: #350 Intersection of Two Arrays II → mastered

--- src/main/java/leetcode/p0350_intersection_of_two_arrays_ii/Solution.java
+++ src/main/java/leetcode/p0350_intersection_of_two_arrays_ii/Solution.java
@@ -43,8 +43,29 @@
  */
 package leetcode.p0350_intersection_of_two_arrays_ii;
 
+import java.util.*;
+
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        return null;
+        int k = 0;
+        int tmp = 0;
+        List<Integer> list = new ArrayList<>(); 
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = k; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    tmp = nums2[j];
+                    nums2[j] = nums2[k];
+                    nums2[k] = tmp;
+                    k++;
+                    break;
+                }
+            }
+        }
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = nums2[i];
+        }
+        return result;
     }
 }
