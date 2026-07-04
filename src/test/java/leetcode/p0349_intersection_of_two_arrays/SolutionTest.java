35426464dcfa3bc42860c0c2a197e5053dbd5663
Tomoki <tomozou4495@gmail.com>
2026-07-04 11:20:19 +0900

    fix: make #349 test order-independent

--- src/test/java/leetcode/p0349_intersection_of_two_arrays/SolutionTest.java
+++ src/test/java/leetcode/p0349_intersection_of_two_arrays/SolutionTest.java
@@ -1,5 +1,6 @@
 package leetcode.p0349_intersection_of_two_arrays;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,13 +12,21 @@ class SolutionTest {
     void example1() {
         // Input:  nums1 = [1,2,2,1], nums2 = [2,2]
         // Output: [2]
-        assertArrayEquals(new int[]{2}, solution.intersection(new int[]{1,2,2,1}, new int[]{2,2}));
+        assertIntersectionEquals(new int[]{2}, solution.intersection(new int[]{1,2,2,1}, new int[]{2,2}));
     }
 
     @Test
     void example2() {
         // Input:  nums1 = [4,9,5], nums2 = [9,4,9,8,4]
         // Output: [9,4]
-        assertArrayEquals(new int[]{9,4}, solution.intersection(new int[]{4,9,5}, new int[]{9,4,9,8,4}));
+        assertIntersectionEquals(new int[]{9,4}, solution.intersection(new int[]{4,9,5}, new int[]{9,4,9,8,4}));
+    }
+
+    private void assertIntersectionEquals(int[] expected, int[] actual) {
+        int[] sortedExpected = Arrays.copyOf(expected, expected.length);
+        int[] sortedActual = Arrays.copyOf(actual, actual.length);
+        Arrays.sort(sortedExpected);
+        Arrays.sort(sortedActual);
+        assertArrayEquals(sortedExpected, sortedActual);
     }
 }
