da66b9b93da644bef3bd612bad37dfb015d390f5
Tomoki <tomozou4495@gmail.com>
2026-07-09 21:26:49 +0900

    scaffold: #389 Find the Difference

---
+++ src/test/java/leetcode/p0389_find_the_difference/SolutionTest.java
@@ -0,0 +1,23 @@
+package leetcode.p0389_find_the_difference;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+// Find the Difference [Easy]
+class SolutionTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    void example1() {
+        // Input:  s = "abcd", t = "abcde"
+        // Output: "e"
+        // assertEquals(expected, solution.findTheDifference(...));
+    }
+
+    @Test
+    void example2() {
+        // Input:  s = "", t = "y"
+        // Output: "y"
+        // assertEquals(expected, solution.findTheDifference(...));
+    }
+}
