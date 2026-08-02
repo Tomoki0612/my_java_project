9b8dad4ae8ea769fd44a389f1d0ad5953a044237
Tomoki <tomozou4495@gmail.com>
2026-08-02 14:37:27 +0900

    progress: #203 Remove Linked List Elements (easy)

--- src/main/java/leetcode/p0203_remove_linked_list_elements/Solution.java
+++ src/main/java/leetcode/p0203_remove_linked_list_elements/Solution.java
@@ -53,11 +53,9 @@ class Solution {
         while (head != null && head.val == val) {
             head = head.next;
         }
-        if (head == null) {
-            return head;
-        }
+
         ListNode cur = head;
-        while (cur.next != null) {
+        while (cur != null && cur.next != null) {
             if (cur.next.val == val) {
                 cur.next = cur.next.next;
             } else {
