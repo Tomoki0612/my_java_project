bb2da0f24c48f0b3ec227289f997cd73621a9fc1
Tomoki <tomozou4495@gmail.com>
2026-08-25 19:25:28 +0900

    progress: #506 Relative Ranks (good)

---
+++ src/main/java/leetcode/p0506_relative_ranks/Solution.java
@@ -0,0 +1,89 @@
+/*
+ * Relative Ranks
+ * Difficulty: Easy
+ * https://leetcode.com/problems/relative-ranks/
+ *
+ * You are given an integer array `score` of size `n`, where `score[i]` is the
+ * score of the `ith` athlete in a competition. All the scores are guaranteed
+ * to be unique.
+ *
+ * The athletes are placed based on their scores, where the `1st` place
+ * athlete has the highest score, the `2nd` place athlete has the `2nd`
+ * highest score, and so on. The placement of each athlete determines their
+ * rank:
+ *
+ * 	  - The `1st` place athlete's rank is `"Gold Medal"`.
+ *
+ * 	  - The `2nd` place athlete's rank is `"Silver Medal"`.
+ *
+ * 	  - The `3rd` place athlete's rank is `"Bronze Medal"`.
+ *
+ * 	  - For the `4th` place to the `nth` place athlete, their rank is their
+ * placement number (i.e., the `xth` place athlete's rank is `"x"`).
+ *
+ * Return an array `answer` of size `n` where `answer[i]` is the rank of the
+ * `ith` athlete.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: score = [5,4,3,2,1]
+ * Output: ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+ * Explanation: The placements are [1st, 2nd, 3rd, 4th, 5th].
+ *
+ * Example 2:
+ *
+ * Input: score = [10,3,8,9,4]
+ * Output: ["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+ * Explanation: The placements are [1st, 5th, 3rd, 2nd, 4th].
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 	  - `n == score.length`
+ *
+ * 	  - `1 <= n <= 104`
+ *
+ * 	  - `0 <= score[i] <= 106`
+ *
+ * 	  - All the values in `score` are unique.
+ */
+package leetcode.p0506_relative_ranks;
+
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        int tmp;
+        int[] rank = score.clone();
+        for (int i = 0; i < rank.length; i++) {
+            for (int j = i; j < rank.length; j++) {
+                if (rank[i] < rank[j]) {
+                    tmp = rank[i];
+                    rank[i] = rank[j];
+                    rank[j] = tmp;
+                }
+            }
+        }
+
+        String[] str = new String[score.length];
+        for (int i = 0; i < str.length; i++) {
+            for (int j = 0; j < rank.length; j++) {
+                if (score[i] == rank[j]) {
+                    if (j == 0) {
+                        str[i] = "Gold Medal";
+                    } else if (j == 1){
+                        str[i] = "Silver Medal";
+                    } else if (j == 2){
+                        str[i] = "Bronze Medal";
+                    } else {
+                        str[i] = Integer.toString(j + 1);
+                    }
+                    
+                }
+            }
+             
+        }
+        return str;
+    }
+}
