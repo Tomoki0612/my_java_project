dd088b6e8f72d0f17f6fa05f9dec980f2a5aee6d
Tomoki <tomozou4495@gmail.com>
2026-09-01 19:41:13 +0900

    progress: #5 Longest Palindromic Substring (good)

--- src/main/java/leetcode/p0005_longest_palindromic_substring/Solution.java
+++ src/main/java/leetcode/p0005_longest_palindromic_substring/Solution.java
@@ -30,6 +30,34 @@ package leetcode.p0005_longest_palindromic_substring;
 
 class Solution {
     public String longestPalindrome(String s) {
-        return null;
+        int maxStart = 0;
+        int maxLength = 1;
+        for (int i = 0; i < s.length(); i++) {
+            int left = i;
+            int right = i;
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                int length = right - left + 1;
+                if (maxLength <= length) {
+                    maxLength = length;
+                    maxStart = left;
+                }
+                left--;
+                right++;
+            }
+
+            left = i;
+            right = i + 1;
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                int length = right - left + 1;
+                if (maxLength <= length) {
+                    maxLength = length;
+                    maxStart = left;
+                }
+                left--;
+                right++;
+            }
+        }
+        return s.substring(maxStart, maxStart + maxLength);
+        
     }
 }
